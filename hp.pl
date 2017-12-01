652180278b2d8d3236406b060fa7baf0e37bc6e6
Elizabeth Siegle <lizzie.siegle@gmail.com>
2017-11-30 22:06:28 -0500

    pre-yhack, need to do last part

---
+++ hp.pl
@@ -0,0 +1,25 @@
+% relations
+% female(person) % person is female
+%  ; to generate all responses.
+female(cedrella).
+female(molly_p).
+female(apolline).
+female(gabrielle).
+female(fleur).
+female(marge).
+female(petunia).
+female(lily_e).
+female(petunia).
+female(lily_p).
+female(ginny).
+female(hermione).
+female(angelina).
+female(rose).
+female(roxane).
+female(audrey).
+female(victoire).
+female(dominique).
+female(molly_w).
+female(lucy).
+
+% male(person) % person is male
